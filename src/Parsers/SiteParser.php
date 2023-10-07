6d2f09af3f47900a10e2554a63f1bedde657a03c
Mahdi Hazaveh <mahdi@hazaveh.net>
2023-10-07 14:10:17 +0200

    use real browser agents to view the url

--- src/Parsers/SiteParser.php
+++ src/Parsers/SiteParser.php
@@ -93,7 +93,12 @@ class SiteParser implements ParserInterface
     public function client(): ClientInterface
     {
         if (! $this->httpClient) {
-            $this->httpClient = new Client(['http_errors' => false]);
+            $this->httpClient = new Client([
+                'http_errors' => false,
+                'headers' => [
+                    'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
+                ]
+            ]);
         }
 
         return $this->httpClient;
